800add07db9922ae7bb6ea38b0fb6dd805f8df42
KaijuML <clement.rebuffel+github@gmail.com>
2019-12-20 19:52:48 +0100

    script to create experiment folder

---
+++ create-experiment.py
@@ -0,0 +1,25 @@
+import pkg_resources
+import argparse
+import shutil
+import os
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Create an experiment folder")
+    parser.add_argument('--name', dest='name', required=True)
+    
+    args = parser.parse_args()
+    
+    folder = pkg_resources.resource_filename(__name__, 'experiments')
+    folder = os.path.join(folder, args.name)
+    
+    if os.path.exists(folder):
+        raise ValueError('An experiment with this name already exists')
+        
+    os.mkdir(folder)
+    os.mkdir(os.path.join(folder, 'data'))
+    os.mkdir(os.path.join(folder, 'models'))
+    os.mkdir(os.path.join(folder, 'gens'))
+    os.mkdir(os.path.join(folder, 'gens', 'test'))
+    os.mkdir(os.path.join(folder, 'gens', 'valid'))
+    
