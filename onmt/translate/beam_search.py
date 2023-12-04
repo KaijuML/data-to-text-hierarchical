67b18b901c172ac52a166942735cf3502f8ebf81
Kedar Thakkar <kedarthakkar@berkeley.edu>
2023-12-03 23:19:19 -0500

    Fix

--- onmt/translate/beam_search.py
+++ onmt/translate/beam_search.py
@@ -187,8 +187,7 @@ class BeamSearch(DecodeStrategy):
         torch.mul(self.topk_scores, length_penalty, out=self.topk_log_probs)
 
         # Resolve beam origin and map to batch index flat representation.
-        print(self.topk_ids.type(), self._batch_index.type())
-        torch.div(self.topk_ids, vocab_size, out=self._batch_index)
+        self._batch_index = torch.div(self.topk_ids, vocab_size, out=self._batch_index.float())
         self._batch_index += self._beam_offset[:_B].unsqueeze(1)
         self.select_indices = self._batch_index.view(_B * self.beam_size)
         self.topk_ids.fmod_(vocab_size)  # resolve true word ids
